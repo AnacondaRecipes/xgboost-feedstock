c83f8cad9d2785de82b1bf077baa824c746889fc
Anthony DiPietro <adipietro@anaconda.com>
2021-11-30 12:00:54 -0500

    Recipes fixes / updates for v1.5.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,15 @@
 
 set -exuo pipefail
 
+# ppc64le does not set this in the compiler packages, yet.
+if [[ ${target_platform} == linux-ppc64le ]]; then
+  CMAKE_ARGS="-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER \
+    -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
+    -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
+    -DCMAKE_FIND_ROOT_PATH=${PREFIX};${BUILD_PREFIX}/${HOST}/sysroot \
+    -DCMAKE_INSTALL_LIBDIR=lib"
+fi
+
 mkdir -p build-target
 pushd build-target
   cmake ${CMAKE_ARGS} \

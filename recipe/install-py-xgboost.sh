c2151a8d2ec5d8126e5cfb1b822a0edca8e77839
Udo-Peter Steyer <ous8292@gmail.com>
2023-01-20 15:45:34 -0600

    adding slash selector statement

--- recipe/install-py-xgboost.sh
+++ recipe/install-py-xgboost.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-
-
 pushd ${SRC_DIR}/python-package
-  ${PYTHON} setup.py install --use-system-libxgboost --single-version-externally-managed --record=record.txt
+  if [[ ${OSTYPE} == msys ]]; then
+    ${PYTHON//\\//} setup.py install --use-system-libxgboost --single-version-externally-managed --record=record.txt
+  else
+    ${PYTHON} setup.py install --use-system-libxgboost --single-version-externally-managed --record=record.txt
+  fi
 popd

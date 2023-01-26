b04c5fe4d6f1202ad53720b115764d95b5dc7add
Andrew Vallette <avallette@anaconda.com>
2023-01-25 17:49:41 -0700

    Removed unnecessary slash conversion for msys2

--- recipe/install-py-xgboost.sh
+++ recipe/install-py-xgboost.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 pushd ${SRC_DIR}/python-package
-  if [[ ${OSTYPE} == msys ]]; then
-    ${PYTHON//\\//} setup.py install --use-system-libxgboost --single-version-externally-managed --record=record.txt
-  else
     ${PYTHON} setup.py install --use-system-libxgboost --single-version-externally-managed --record=record.txt
-  fi
 popd

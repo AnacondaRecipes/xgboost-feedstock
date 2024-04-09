3315dc0068da4d391435237b7977b87eb011c810
Charles Bousseau <cbousseau@anaconda.com>
2024-04-09 00:14:28 -0400

    refresh recipe

--- recipe/install-libxgboost.sh
+++ recipe/install-libxgboost.sh
@@ -1,36 +1,14 @@
 #!/bin/bash
 
+set -exuo pipefail
 
-
-if [[ ${OSTYPE} == msys ]]; then
-  # Just for now; should we handle slash fixing in conda-build?
-  PREFIX=${PREFIX//\\//}
-  SRC_DIR=${SRC_DIR//\\//}
-fi
-
-if [[ ${OSTYPE} == msys ]]; then
-  LIBDIR=${PREFIX}/Library/mingw-w64/lib
-  INCDIR=${PREFIX}/Library/mingw-w64/include
-  BINDIR=${PREFIX}/Library/mingw-w64/bin
-  SODIR=${BINDIR}
-  XGBOOSTDSO=xgboost.dll
-  EXEEXT=.exe
-else
-  LIBDIR=${PREFIX}/lib
-  INCDIR=${PREFIX}/include
-  BINDIR=${PREFIX}/bin
-  SODIR=${LIBDIR}
-  if [[ $(uname) == Darwin ]]; then
-    XGBOOSTDSO=libxgboost.dylib
-  else
-    XGBOOSTDSO=libxgboost.so
-  fi
-  EXEEXT=
-fi
-
-mkdir -p ${LIBDIR} ${INCDIR}/xgboost ${BINDIR} || true
-cp ${SRC_DIR}/xgboost${EXEEXT} ${BINDIR}/
-cp ${SRC_DIR}/lib/${XGBOOSTDSO} ${SODIR}/
-cp -Rf ${SRC_DIR}/include/xgboost ${INCDIR}/
-cp -Rf ${SRC_DIR}/rabit/include/rabit ${INCDIR}/xgboost/
-cp -f ${SRC_DIR}/src/c_api/*.h ${INCDIR}/xgboost/
+mkdir -p build-target
+pushd build-target
+  cmake ${CMAKE_ARGS} \
+    -GNinja \
+    -DCMAKE_BUILD_TYPE:STRING="Release" \
+    -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
+    -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+    "${SRC_DIR}"
+  ninja
+popd
---
+++ recipe/install-py-xgboost.bat
@@ -0,0 +1,4 @@
+@echo on
+pushd "%SRC_DIR%"\python-package
+%PYTHON% -m pip install --no-deps --no-build-isolation . -vv
+popd
